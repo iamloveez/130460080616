5d5783c2919c4451dfd7404812b5fe15bfcf4ad0
iamloveez <iamloveez@gmail.com>
2017-06-16 03:01:30 -0400

    .

--- new_10346008_0616/app/controllers/candidates_controller.rb
+++ new_10346008_0616/app/controllers/candidates_controller.rb
@@ -15,9 +15,30 @@ class CandidatesController < ApplicationController
         # 失敗
         render :new
       end
+    end 
+    def edit
+      @candidate = Candidate.find_by(id: params[:id])
     end
+    def update
+      @candidate = Candidate.find_by(id: params[:id])
+
+      if @candidate.update(candidate_params)
+        # 成功
+        redirect_to candidates_path, notice: "資料更新成功!"
+      else
+        # 失敗
+        render :edit
+      end
+    end
+    def destroy
+      @candidate = Candidate.find_by(id: params[:id])
+      @candidate.destroy if @candidate
+      redirect_to candidates_path, notice: "資料已刪除!"
+    end
+  
     private
     def candidate_params
       params.require(:candidate).permit(:name, :student, :score)
     end
+  
 end
